556f3f4cc7f951b09efe5a7a252fc9a50d69d90d
galich2 <galich2@mail.ru>
2019-11-13 11:51:01 +0400

    131119

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,21 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 90);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 233);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(290, 90);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 233);
+            this.textBox2.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(565, 353);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "MainForm";
             this.Text = "Морской бой";
-            
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -16,6 +16,61 @@ namespace Морской_Бой
         {
             InitializeComponent();
         }
-
+        Море sea = new Море();
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //Сброс();
+            sea.ПоставитьКорабль(0, new Program.Точка[]
+            {
+                new Program.Точка(0,1),
+                new Program.Точка(0,2),
+                new Program.Точка(0,3),
+                new Program.Точка(0,4),
+            });
+            ShowSea();
+            sea.ПоставитьКорабль(1, new Program.Точка[]
+            {
+                new Program.Точка(2,1),
+                new Program.Точка(2,2),
+                new Program.Точка(2,3),
+            });
+            sea.Выстрел(new Program.Точка(0,1));
+            ShowSea();
+            ShowFight();
+        }
+        public void ShowSea()
+        {
+            string text = "";
+            for (int y = 0; y < Море.Размер_моря.y; y++)
+            {
+                for (int x = 0; x < Море.Размер_моря.x; x++)
+                    text += sea.КартаКораблей(new Program.Точка(x, y)) == -1 ? "." : "O";
+                text += Environment.NewLine;
+            }
+            textBox1.Text = text;
+        }
+        public void ShowFight()
+        {
+            string text = "";
+            for (int y = 0; y < Море.Размер_моря.y; y++)
+            {
+                for (int x = 0; x < Море.Размер_моря.x; x++)
+                {
+                    switch (sea.КартаПопаданий(new Program.Точка(x, y)))
+                    {
+                        case Program.Статус.Неизвестно: 
+                            text += "."; break;
+                        case Program.Статус.Мимо:
+                            text += ","; break;
+                        case Program.Статус.Ранил:
+                            text += "x"; break;
+                        case Program.Статус.Убил:
+                            text += "X"; break;
+                    }
+                }
+                text += Environment.NewLine;
+            }
+            textBox2.Text = text;
+        }
     }
 }

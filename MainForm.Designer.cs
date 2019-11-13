613bcde38e5d0d0b78631ad6ac71390fb71994ab
Alexandr Glebov <galich2@mail.ru>
2019-11-13 19:28:46 +0400

    13244

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,47 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.grid_user = new System.Windows.Forms.DataGridView();
+            this.grid_comp = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_user)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_comp)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // grid_user
             // 
-            this.textBox1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(12, 90);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(252, 233);
-            this.textBox1.TabIndex = 0;
+            this.grid_user.AllowUserToAddRows = false;
+            this.grid_user.AllowUserToDeleteRows = false;
+            this.grid_user.AllowUserToOrderColumns = true;
+            this.grid_user.AllowUserToResizeColumns = false;
+            this.grid_user.AllowUserToResizeRows = false;
+            this.grid_user.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grid_user.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid_user.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grid_user.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_user.Location = new System.Drawing.Point(12, 40);
+            this.grid_user.Name = "grid_user";
+            this.grid_user.RowHeadersWidth = 50;
+            this.grid_user.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.grid_user.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.grid_user.ShowEditingIcon = false;
+            this.grid_user.Size = new System.Drawing.Size(266, 279);
+            this.grid_user.TabIndex = 0;
             // 
-            // textBox2
+            // grid_comp
             // 
-            this.textBox2.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(290, 90);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(252, 233);
-            this.textBox2.TabIndex = 1;
+            this.grid_comp.AllowUserToAddRows = false;
+            this.grid_comp.AllowUserToDeleteRows = false;
+            this.grid_comp.AllowUserToResizeColumns = false;
+            this.grid_comp.AllowUserToResizeRows = false;
+            this.grid_comp.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grid_comp.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid_comp.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.grid_comp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_comp.Location = new System.Drawing.Point(287, 40);
+            this.grid_comp.Name = "grid_comp";
+            this.grid_comp.RowHeadersWidth = 50;
+            this.grid_comp.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.grid_comp.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.grid_comp.ShowEditingIcon = false;
+            this.grid_comp.Size = new System.Drawing.Size(266, 279);
+            this.grid_comp.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(565, 353);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.grid_comp);
+            this.Controls.Add(this.grid_user);
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Морской бой";
-            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid_user)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_comp)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView grid_user;
+        private System.Windows.Forms.DataGridView grid_comp;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -12,65 +12,33 @@ namespace Морской_Бой
 {
     public partial class MainForm : Form
     {
+        Море sea_user = new Море();
+        Море sea_comp = new Море();
+        static string abc = "АБВГДЕЖЗИК";
+        Color Color_back = Color.DarkSeaGreen;
         public MainForm()
         {
             InitializeComponent();
+            sea_user = new Море();
+            sea_comp = new Море();
+            InitGrid(grid_user);
+            InitGrid(grid_comp);
+
         }
-        Море sea = new Море();
-        private void MainForm_Load(object sender, EventArgs e)
+      private void InitGrid(DataGridView grid)
         {
-            //Сброс();
-            sea.ПоставитьКорабль(0, new Program.Точка[]
+            grid.Rows.Clear();
+            grid.Columns.Clear();
+            grid.DefaultCellStyle.BackColor = Color_back;
+            for (int x = 0; x < Море.Размер_моря.x; x++)  
+                grid.Columns.Add("col_" + x.ToString(), abc.Substring(x, 1));
+            for (int y = 0; y < Море.Размер_моря.x; y++)
             {
-                new Program.Точка(0,1),
-                new Program.Точка(0,2),
-                new Program.Точка(0,3),
-                new Program.Точка(0,4),
-            });
-            ShowSea();
-            sea.ПоставитьКорабль(1, new Program.Точка[]
-            {
-                new Program.Точка(2,1),
-                new Program.Точка(2,2),
-                new Program.Точка(2,3),
-            });
-            sea.Выстрел(new Program.Точка(0,1));
-            ShowSea();
-            ShowFight();
-        }
-        public void ShowSea()
-        {
-            string text = "";
-            for (int y = 0; y < Море.Размер_моря.y; y++)
-            {
-                for (int x = 0; x < Море.Размер_моря.x; x++)
-                    text += sea.КартаКораблей(new Program.Точка(x, y)) == -1 ? "." : "O";
-                text += Environment.NewLine;
+                grid.Rows.Add();
+                grid.Rows[y].HeaderCell.Value = (y + 1).ToString();
             }
-            textBox1.Text = text;
+            grid.Height = Море.Размер_моря.y * grid.Rows[0].Height + grid.ColumnHeadersHeight + 2;
         }
-        public void ShowFight()
-        {
-            string text = "";
-            for (int y = 0; y < Море.Размер_моря.y; y++)
-            {
-                for (int x = 0; x < Море.Размер_моря.x; x++)
-                {
-                    switch (sea.КартаПопаданий(new Program.Точка(x, y)))
-                    {
-                        case Program.Статус.Неизвестно: 
-                            text += "."; break;
-                        case Program.Статус.Мимо:
-                            text += ","; break;
-                        case Program.Статус.Ранил:
-                            text += "x"; break;
-                        case Program.Статус.Убил:
-                            text += "X"; break;
-                    }
-                }
-                text += Environment.NewLine;
-            }
-            textBox2.Text = text;
-        }
+       
     }
 }
